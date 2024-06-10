3df009a0aea82fd750f307b3edff544c5bb259fd
shucksow <102425362+shucksow@users.noreply.github.com>
2024-06-10 15:25:41 -0400

    feat: Create human and computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const gameChoices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    return gameChoices[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+    while(true) {
+        let humanChoice = prompt("Rock, Paper, or Scissors?")
+        humanChoice = humanChoice.toLowerCase;
+    
+        if (humanChoice === 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
+            break;
+        }
+    }
+    
+    return humanChoice;
+ }
