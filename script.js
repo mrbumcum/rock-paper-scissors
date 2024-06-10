a66dbc5f771287db1b1e28901420969568c7e173
shucksow <102425362+shucksow@users.noreply.github.com>
2024-06-10 15:43:29 -0400

    feat: Add game logic of Rock Paper Scissors into playRound()

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ function getHumanChoice() {
     return humanChoice;
  }
 
+ function playRound(getComputerChoice, getHumanChoice) {
+    if (
+        (getComputerChoice() === "rock" && getHumanChoice() === "scissor")||
+        (getComputerChoice() === "paper" && getHumanChoice() === "rock") ||
+        (getComputerChoice() === "scissor" && getHumanChoice() === "paper")
+    ) {
+        computerScore++;
+        console.log(`You lose! ${getComputerChoice()} beats ${getHumanChoice}`)
+    } else if (getComputerChoice() === getHumanChoice) {
+        console.log("It's a draw!")
+    } else {
+        humanScore++;
+        console.log(`You win! ${getHumanChoice()} beats ${getComputerChoice}`)
+    }
+ }
+
 let humanScore = 0;
 let computerScore = 0;
+
+
 
