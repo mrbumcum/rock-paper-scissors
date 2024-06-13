1c41eeb0345c1ef62f4f70e1baa5db60150ed40b
shucksow <102425362+shucksow@users.noreply.github.com>
2024-06-13 13:22:38 -0400

    feat: EventListener for RPS logic

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ function getComputerChoice() {
     return gameChoices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    while(true) {
-        let humanChoice = prompt("Rock, Paper, or Scissors?")
-        humanChoice = humanChoice.toLowerCase();
-    
-        if (humanChoice === 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
-            return humanChoice;
-        }
-    }
- }
-
- function playRound() {
+ function playRound(humanChoice) {
     const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
+    const pointHeader = document.querySelector("div.scoreboard h3.header-font");
+    const computerChoiceImg = document.querySelector("#computerImg");
+    const humanChoiceImg = document.querySelector("#humanImg");
+    const humanScoreP = document.querySelector("#humanScore");
+    const computerScoreP =document.querySelector("#computerScore");
+
+    computerChoiceImg.textContent = document.querySelector("#" + computerChoice + "Btn").textContent;
+    humanChoiceImg.textContent = document.querySelector("#" + humanChoice + "Btn").textContent;
+
 
     if (
         (computerChoice === "rock" && humanChoice === "scissors")||
@@ -25,32 +22,50 @@ function getHumanChoice() {
         (computerChoice === "scissors" && humanChoice === "paper")
     ) {
         computerScore++;
-        console.log(`You lose! ${humanChoice} beats ${humanChoice}`)
-    } else if (computerChoice === getHumanChoice) {
-        console.log("It's a draw!")
+        pointHeader.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+    } else if (computerChoice === humanChoice) {
+        pointHeader.textContent = "It's a draw!";
     } else {
         humanScore++;
-        console.log(`You win! ${computerChoice} beats ${computerChoice}`)
+         pointHeader.textContent = `You win! ${computerChoice} beats ${computerChoice}`;
     }
+
+    humanScoreP.textContent = humanScore;
+    computerScoreP.textContent = computerScore;
  }
 
- function playGame(triesAmount) {
-    humanScore = 0;
-    computerScore = 0;
+ function playGame(humanChoice) {
 
-    for(let i = 0; i < triesAmount; i++) {
-        playRound();
+    const finalGameHeader = document.querySelector(".scoreboard h3.header-font");
+    
+    playRound(humanChoice);
+
+    if (humanScore === 5 || computerScore === 5) {
+        console.log("Final Scores:");
+        console.log("Human Scores", humanScore);
+        console.log("Computer Score", computerScore);
+
+        if (humanScore > computerScore) {
+            finalGameHeader.textContent = "Congratulations! You won the game.";
+        } else if (humanScore === computerScore) {
+            finalGameHeader.textContent = "The game ended in a tie!";
+        } else {
+            finalGameHeader.textContent = "Oh no! The computer won the game.";
+        }  
+
+        humanScore = 0;
+        computerScore = 0;
     }
 
-    console.log("Final Scores:");
-    console.log("Human Scores", humanScore);
-    console.log("Computer Score", computerScore);
-
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You won.");
-    } else if (humanScore === computerScore) {
-        console.log("It's a tie!")
-    } else {
-        console.log("Oh no! The computer won.")
-    }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector('#rockBtn');
+const paperBtn = document.querySelector('#paperBtn');
+const scissorsBtn = document.querySelector('#scissorsBtn');
+ 
+rockBtn.addEventListener("click", () => playGame("rock"));
+paperBtn.addEventListener("click", () => playGame("paper"));
+scissorsBtn.addEventListener("click", () => playGame("scissors"));
